117686725123a00ae3b0f6c5b2b9bd17f2394c9e
jsarria-tableau <jsarria@tableau.com>
2018-11-27 11:11:40 -0800

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Imma testin'!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
